03bc875fc32291fcf0f735d81e9fd4ee87b329a9
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2020-10-08 15:41:03 +0200

    ci: create directory recursively

--- scripts/ci/compile-widgets.js
+++ scripts/ci/compile-widgets.js
@@ -5,13 +5,12 @@
 
 // Modules.
 const config = require('config');
-const { mkdir, copyFile } = require('fs');
+const { mkdir } = require('fs');
 const { resolve: resolvePath, join: joinPath } = require('path');
 const { promisify } = require('util');
 const rimraf = require('rimraf');
 
 const compileWidgets = require('@js-widgets/compiler');
-const copyFileP = promisify(copyFile);
 const mkdirP = promisify(mkdir);
 const rimrafP = promisify(rimraf);
 
@@ -37,7 +36,7 @@ const distDir = joinPath(
   try {
     // Delete the existing dist directory and re-create it.
     await rimrafP(distDir);
-    await mkdirP(distDir);
+    await mkdirP(distDir, { recursive: true });
 
     // Compile all the widgets and produce the registry.json.
     const message = await compileWidgets(options);
