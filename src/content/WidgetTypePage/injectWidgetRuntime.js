6f12da349c4ca6e3cd3ed992b3c2d4d6d02808ae
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2021-01-25 23:12:38 +0100

    feat: include CSS file to the page

--- src/content/WidgetTypePage/injectWidgetRuntime.js
+++ src/content/WidgetTypePage/injectWidgetRuntime.js
@@ -5,14 +5,32 @@ export default function (widget, htmlId) {
   if (document.getElementById(elementId)) {
     return Promise.resolve().then(() => renderWidget(widget, htmlId));
   }
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.id = elementId;
-    script.src = `${widget.directoryUrl}/js/main.js`;
-    script.crossorigin = 'anonymous';
-    script.async = true;
-    script.onload = resolve;
-    script.onerror = reject;
-    document.body.appendChild(script);
-  }).then(() => renderWidget(widget, htmlId));
+  const assetPromises = [
+    new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.id = elementId;
+      script.src = `${widget.directoryUrl}/js/main.js`;
+      script.crossorigin = 'anonymous';
+      script.async = true;
+      script.onload = resolve;
+      script.onerror = reject;
+      document.body.appendChild(script);
+    }),
+  ];
+  if (widget.files.includes('css/main.css')) {
+    assetPromises.push(
+      new Promise((resolve, reject) => {
+        const link = document.createElement('link');
+        link.crossOrigin = 'anonymous';
+        link.type = 'text/css';
+        link.media = 'all';
+        link.rel = 'stylesheet';
+        link.href = `${widget.directoryUrl}/css/main.css`;
+        link.onload = resolve;
+        link.onerror = reject;
+        document.head.appendChild(link);
+      }),
+    );
+  }
+  return Promise.all(assetPromises).then(() => renderWidget(widget, htmlId));
 }
